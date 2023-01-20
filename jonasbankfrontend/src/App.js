96a9759627c8bc7f5373c214f6e0952633a19fb7
Jonas324 <jonaswadin24@gmail.com>
2023-01-20 14:34:54 +0100

    connected database

--- jonasbankfrontend/src/App.js
+++ jonasbankfrontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8082/user/1")
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  }, []);
+  console.log(user);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jonas Bank app</h1>
+      <div className="chatwindow">
+        <p>username: {user.username}</p>
+        <p>user ID: {user.id}</p>
+        <p>credit: {user.credit}</p>
+      </div>
     </div>
   );
 }
-
 export default App;
